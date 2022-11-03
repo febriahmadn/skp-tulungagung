36e06ec61cf87b5c6a859593fd08af48a921e283
Roni Ahmadi <roniahmadi30@gmail.com>
2022-11-03 15:56:23 +0700

    feat: register perspektif

--- skp/admin.py
+++ skp/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 
 #model import 
-from .models import DetailSasaranKinerja, RencanaHasilKerja
+from .models import DetailSasaranKinerja, RencanaHasilKerja, Perspektif
 
 #admin Import 
 from .subadmin.detailsasarankerja_admin import DetailsasarankerjaAdmin
 from .subadmin.rencanahasilkerja_admin import RencanahasilkerjaAdmin
+from .subadmin.perspektif_admin import PerspektifAdmin
 
 
 
 admin.site.register(DetailSasaranKinerja, DetailsasarankerjaAdmin)
 admin.site.register(RencanaHasilKerja, RencanahasilkerjaAdmin)
+admin.site.register(Perspektif, PerspektifAdmin)
---
+++ skp/subadmin/perspektif_admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+
+class PerspektifAdmin(admin.ModelAdmin):
+    list_display=('pk','perspektif','created')
