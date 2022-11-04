18121fbd84dac5b379bca7ae69fc01d4d7b44f33
Roni Ahmadi <roniahmadi30@gmail.com>
2022-11-04 11:03:50 +0700

    feat: register admin model indikator

--- skp/admin.py
+++ skp/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 
 #model import 
-from .models import DetailSasaranKinerja, RencanaHasilKerja, Perspektif
+from .models import DetailSasaranKinerja, RencanaHasilKerja, Perspektif, IndikatorKinerjaIndividu
 
 #admin Import 
 from .subadmin.detailsasarankerja_admin import DetailsasarankerjaAdmin
 from .subadmin.rencanahasilkerja_admin import RencanahasilkerjaAdmin
 from .subadmin.perspektif_admin import PerspektifAdmin
+from .subadmin.indikator_admin import IndikatorAdmin
 
 
 
 admin.site.register(DetailSasaranKinerja, DetailsasarankerjaAdmin)
 admin.site.register(RencanaHasilKerja, RencanahasilkerjaAdmin)
 admin.site.register(Perspektif, PerspektifAdmin)
+admin.site.register(IndikatorKinerjaIndividu, IndikatorAdmin)
---
+++ skp/subadmin/indikator_admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+
+class IndikatorAdmin(admin.ModelAdmin):
+    list_display=('pk','rencana_kerja','indikator','target','perspektif')
