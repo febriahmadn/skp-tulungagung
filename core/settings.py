3c1b90947365f446627e0c202b05a9b3a03d13ed
Febri Ahmad Nurhidayat <febriahmadn@gmail.com>
2022-11-15 22:11:37 +0700

    chore setting url status

--- core/settings.py
+++ core/settings.py
@@ -152,8 +152,16 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
-STATIC_ROOT = os.path.join(BASE_DIR, "metronic/static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'files/static-collected/')
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'files/media/')
+
 
 
 # Default primary key field type
